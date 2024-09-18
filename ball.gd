c7247aa344f18e30ee6b7eee4a8b0cb1f4056ebc
michaelcorduroy <cellben99@gmail.com>
2024-09-17 21:05:19 -0400

    added a clank

--- ball.gd
+++ ball.gd
@@ -2,10 +2,15 @@ extends RigidBody3D
 
 @export var counter = 0
 
+@onready var audio = self.get_parent().get_child(0)
+
+var clank = preload("res://clank.mp3")
+
 func _on_body_entered(body):
 	# Check if the colliding body is a StaticBody3D (or any other type you want)
 	if body.is_class("StaticBody3D"):
 		self.counter += 1
+		audio.play()
 		print("bounced: " + str(self.counter))
 
 		# Create a duplicate of the current RigidBody3D with a unique name
